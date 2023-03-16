8ffb020d6dc196b2f06cd278eb88e6b6fd40c1be
jeevs100 <rishabjiwani04@gmail.com>
2023-03-16 17:51:14 +0530

    Initial commit

---
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,20 @@
+package com.luv2code.springboot.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappApplication.class, args);
+	}
+
+}
+
+/* RestContoller
+ * 
+ * @RestController --> set up rest controller
+ * 
+ *  @GetMapping("/") -->handle http get requests
+ * */
